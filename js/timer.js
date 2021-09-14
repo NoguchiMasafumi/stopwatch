cf1f16351633f84bfd448fff30b840cbf057b423
NoguchiMasafumi <89947969+NoguchiMasafumi@users.noreply.github.com>
2021-09-14 10:38:54 +0900

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -2,8 +2,7 @@ var stopwatchStartTime;
 var stopwatchState = 0;
 var stopwatchTotal = 0;
 var stopwatchTimerID = 0;
-function setStopWatchText()
-{
+function setStopWatchText(){
 	if(stopwatchState == 1){
 		var time = getStopWatchTime();
 		var t = String(parseInt(time / 10));
@@ -20,21 +19,20 @@ function setStopWatchText()
 	}
 
 }
-function getStopWatchTime()
-{
+
+function getStopWatchTime(){
 	var temp = new Date();
 	return (temp - stopwatchStartTime) + stopwatchTotal;
 }
-function stopwatch_click(id)
-{
+
+function stopwatch_click(id){
 	if(id == 0){
 		switch(stopwatchState){
-			case 0: //停止中
-				document.stopwatch_form.stopwatch_button.value = '停止';
-				stopwatchState = 1;
+			case 0: //開始ボタン状態
 				stopwatchStartTime = new Date();
 				stopwatchTimerID = setInterval("setStopWatchText()",10);
-
+				document.stopwatch_form.stopwatch_button.value = '停止';
+				stopwatchState = 1;
 				break;
 			case 1: //動作中
 				if(stopwatchTimerID){
@@ -50,9 +48,8 @@ function stopwatch_click(id)
 		if(stopwatchState == 0){
 			document.stopwatch_form.stopwatch_button.value = '開始';
 		}
-//		document.stopwatch_form.stopwatch_text.value = '	   0.00';
 		document.getElementById("stopwatch_text").innerHTML = '0.00';
-	stopwatchTotal = 0;
+		stopwatchTotal = 0;
 	}
 }
 
